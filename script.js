8dac9e0b8e9523fad5c51edb8b4c9f27be709aee
BowieRC <rowlandscrispebowie@gmail.com>
2023-09-28 17:20:14 +0930

    create functions to add selected character to password string

--- script.js
+++ script.js
@@ -41,6 +41,166 @@ function getCriteria(){
   }
 }
 
+function findNextCharacter(){
+  for(i=0; i<requiredLength; i++){
+    switch(criteriaNumber){
+      case(1):
+        oneCriteria();
+        setCharacter();
+        break;
+      case(2):
+        twoCriteria();
+        setCharacter();
+        break;
+      case(3):
+        threeCriteria();
+        setCharacter(); 
+        break;
+      case(4):
+        fourCriteria();
+        setCharacter();
+        break;
+    }
+    
+  }
+//     console.log(characterString)
+  
+  
+
+//   console.log(nextCharacter);
+  
+}
+
+// Find what the next character will be
+
+function oneCriteria(){
+  if(needsLowercase){
+        characterString = lowercaseString;
+      }
+      if(needsUppercase){
+         characterString = uppercaseString;
+      }
+      if(needsSpecial){
+        characterString = specialString;
+      }
+      if(needsNumbers){
+        characterString = numberString;
+      }
+}
+
+function twoCriteria(){
+  chance = Math.floor(Math.random() * 10);
+  if(needsUppercase && needsLowercase){
+    if(chance >5){
+      characterString = uppercaseString;
+      }
+    else{
+      characterString = lowercaseString;
+    }
+  }
+  else if(needsUppercase && needsSpecial){
+    if(chance >5){
+      characterString = uppercaseString;
+      }
+    else{
+      characterString = lowercaseString;
+        }
+      }
+  else if(needsUppercase && needsNumbers){
+    if(chance >5){
+      characterString = uppercaseString;
+      }
+    else{
+      characterString = numberString;
+    }
+  }
+  else if(needsLowercase && needsSpecial){
+    if(chance >5){
+      characterString = lowercaseString;
+      }
+    else{
+      characterString = specialString;
+    }
+  }
+  else if(needsLowercase && needsNumbers){
+    if(chance >5){
+      characterString = lowercaseString;
+      }
+    else{
+      characterString = numberString;
+    }
+  }
+  else if(needsSpecial && needsNumbers){
+    if(chance >5){
+      characterString = specialString;
+      }
+    else{
+      characterString = numberString;
+    }
+  }
+}
+
+function threeCriteria(){
+  chance = Math.floor(Math.random() * 9);
+  if(!needsUppercase)
+    if(chance <3){
+        characterString = lowercaseString;
+        }
+      else if(chance >=3 && chance <6){
+        characterString = numberString;
+      }
+      else{
+        characterString = specialString;
+      }
+  if(!needsLowercase){
+    if(chance <3){
+      characterString = UppercaseString;
+      }
+    else if(chance >=3 && chance <6){
+      characterString = numberString;
+    }
+    else{
+      characterString = specialString;
+    }
+  }
+  if(!needsSpecial){
+    if(chance <3){
+      characterString = lowercaseString;
+      }
+    else if(chance >=3 && chance <6){
+      characterString = numberString;
+    }
+    else{
+      characterString = uppercaseString;
+    }
+      }
+if(!needsNumbers){
+  if(chance <3){
+      characterString = lowercaseString;
+      }
+    else if(chance >=3 && chance <6){
+      characterString = uppercaseString;
+    }
+    else{
+      characterString = specialString;
+    }
+  }
+}
+
+function fourCriteria(){
+  chance = Math.floor(Math.random() * 12);
+  if(chance <3){
+    characterString = uppercaseString;
+  } else if (chance <6 && chance >=3){
+    characterString = lowercaseString;
+  } else if (chance <9 && chance >=6){
+    characterString = specialString;
+  } else{
+    characterString = numberString;
+  }
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", () => {
   passwordRequirements();
